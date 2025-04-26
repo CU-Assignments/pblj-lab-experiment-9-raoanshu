50418b21d9aec915aa563080f5789e0919a42cd1
raoanshu <146465623+raoanshu@users.noreply.github.com>
2025-04-26 10:15:31 +0530

    Update Exp9.1.java

--- Exp9.1.java
+++ Exp9.1.java
@@ -1,1 +1,22 @@
 
+public interface GreetingService {
+    void greet(String name);
+}
+
+
+// 2nd step
+public class GreetingServiceImpl implements GreetingService {
+    public void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
+// third step 
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public GreetingService greetingService() {
+        return new GreetingServiceImpl();
+    }
+}
